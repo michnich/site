43b0f26c35bb8f9437414f66d8cd3e5404f37f0b
Michaela <michnich07@gmail.com>
2016-04-18 23:53:48 -0400

    add volunteers, isEmpty object function added to commmon.js

--- client/common.js
+++ client/common.js
@@ -6,3 +6,17 @@ Meteor.startup(function () {
     passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL',        // One of 'USERNAME_AND_EMAIL', 'USERNAME_AND_OPTIONAL_EMAIL', 'USERNAME_ONLY', or 'EMAIL_ONLY' (default).
   });
 });
+
+
+isEmpty = function (obj) {   
+    if (obj == null) return true;
+   
+    if (obj.length && obj.length > 0)    return false;
+    if (obj.length === 0)  return true;
+ 
+    for (var key in obj) {
+        if (hasOwnProperty.call(obj, key)) return false;
+    }
+ 
+    return true;
+}
