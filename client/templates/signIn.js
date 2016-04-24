e0b443dc79e4fc2bda0c10dbc5053516ce00be95
michnich <michnich07@gmail.com>
2016-04-23 22:32:04 -0400

    Revert "Users"

--- client/templates/signIn.js
+++
@@ -1,4 +0,0 @@
-Template.signIn.onCreated({
-	//post signup hook
-	
-});
--- server/accountsHooks.js
+++
@@ -1,8 +0,0 @@
-//called after successful account creation
-//sets the users permissions
-var assignUserSet = function(userId) {
-	
-};
-AccountsTemplates.configure({
-	postSignUpHook: assignUserSet
-});
