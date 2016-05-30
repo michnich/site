cc82c6ebf09c6464bb04f0d80d378605529bd919
Michaela <michnich07@gmail.com>
2016-05-30 17:23:52 -0400

    student profile pulls contact info from parent

--- client/templates/students/studentProfile.js
+++ client/templates/students/studentProfile.js
@@ -3,15 +3,19 @@ Template.studentProfile.helpers({
         return moment().format("YYYY-MM-DD");
     },
     studentAttendance: function() {
-      Meteor.subscribe("singleStudentAttendance", this._id);
-      var mostRecent = _.toArray(StudentAttendance.find().fetch());
-      var dateStrings = [];
-      var i;
-      var x = (mostRecent.length < 4) ? mostRecent.length : 4;
-      for (i = 0; i < x; i++) {
-        dateStrings.push(moment(mostRecent[i].date).format("MM-DD-YYYY"));
-      }
-      return dateStrings;
+        Meteor.subscribe("singleStudentAttendance", this._id);
+        var mostRecent = _.toArray(StudentAttendance.find().fetch());
+        var dateStrings = [];
+        var i;
+        var x = (mostRecent.length < 4) ? mostRecent.length : 4;
+        for (i = 0; i < x; i++) {
+            dateStrings.push(moment(mostRecent[i].date).format("MM-DD-YYYY"));
+        }
+        return dateStrings;
+    },
+    parent: function(field) {
+        Meteor.subscribe("parentById", this.parentId);
+        return Parents.find();
     }
 });
 
@@ -20,16 +24,16 @@ Template.studentProfile.events({
         e.preventDefault();
         var date = $(e.target).find('[name=attendanceDate]').val();
         if (!this.startDate) {
-          Meteor.call("setStartDate", this._id, date, function(error, result){
-            if(error){
-              console.log("error", error);
-            }
-          });
+            Meteor.call("setStartDate", this._id, date, function(error, result) {
+                if (error) {
+                    console.log("error", error);
+                }
+            });
         }
-        Meteor.call("takeAttendance", date, this._id, function(error, result){
-          if(error){
-            console.log("error", error);
-          }
+        Meteor.call("takeAttendance", date, this._id, function(error, result) {
+            if (error) {
+                console.log("error", error);
+            }
         });
     }
 });
