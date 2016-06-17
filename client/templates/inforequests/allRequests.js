e0cb7397046291bef653b85b08a20decdaf12d42
Michaela <michnich07@gmail.com>
2016-06-17 11:07:55 -0400

    basic filter by type

--- client/templates/inforequests/allRequests.js
+++ client/templates/inforequests/allRequests.js
@@ -1,9 +1,25 @@
 Template.allRequests.onCreated(function() {
-	Meteor.subscribe("allRequests");
+    Meteor.subscribe("allRequests");
+    Session.set("statusType", "")
 });
 
 Template.allRequests.helpers({
-	requests: function() {
-		return InfoRequests.find();
-	}
+    requests: function() {
+        var statusType = Session.get("statusType");
+        if (statusType === "") {
+            return InfoRequests.find({});
+        } else {
+            return InfoRequests.find({
+                status: statusType
+            });
+        }
+    }
 });
+
+Template.allRequests.events({
+    'change #selectStatus': function(e) {
+				e.preventDefault();
+        var statusType = $(e.target).val();
+        Session.set("statusType", statusType);
+    }
+})
