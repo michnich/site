a40fd55c9f167d641edc30f1d515a3ac299a5239
Michaela <michnich07@gmail.com>
2016-07-06 11:24:55 -0400

    edit parent profile form

--- client/templates/parents/editParentProfile.js
+++ client/templates/parents/editParentProfile.js
@@ -62,7 +62,7 @@ Template.editParentProfile.helpers({
         }
     },
     stateSelected: function(state) {
-      if (if this.state === state) {
+      if (this.state === state) {
         return true;
       }
       else {
@@ -80,19 +80,12 @@ Template.editParentProfile.events({
     'submit form': function(e) {
         e.preventDefault();
         //gather all the info from the fields
-        var parentId = this._id;
+        var parentId = Parents.findOne({})._id;
 
         var parentProfile = {
             first_name: $(e.target).find('[name=first_name]').val(),
             last_name: $(e.target).find('[name=last_name]').val(),
             dob: $(e.target).find('[name=dob]').val(),
-            gender: $(e.target).find("[name=gender]:checked").map(function() {
-                if (this.value === "Other") {
-                    return $(e.target).find("[name=otherText]").val();
-                } else {
-                    return this.value;
-                }
-            }).get(),
             race_ethnicity: $(e.target).find("[name=race_ethnicity]:checked").map(function() {
                 return this.value;
             }).get(),
@@ -108,10 +101,10 @@ Template.editParentProfile.events({
             //EMPLOYMENT INFO
         };
 
-        Meteor.call("parentUpdate", parent, parentId, function(error, result) {
+        Meteor.call("parentUpdate", parentProfile, parentId, function(error, result) {
             if (error) {
                 //error handling
-                console.log("error", error);
+                console.log(error);
             }
             if (result) {
                 //do something
